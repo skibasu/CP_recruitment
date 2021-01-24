06afa90801b0d73f437e16a412b4eebaa6a4938a
SuMan <lukaszskiba@MacBook-Pro-SuMan.local>
2021-01-24 18:50:48 +0100

    type added

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import image from "./loader.svg";
-const Loader = () => (
+const Loader: React.FC = () => (
     <div>
         <img src={image} alt="loader" />
     </div>
